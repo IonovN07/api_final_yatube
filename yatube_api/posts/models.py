6e137b61162f4f37faca98c515e06fa2306e2845
Nik Ionov <nhjase@yandex.ru>
2025-05-01 15:46:05 +0500

    Add changes to the project

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -17,12 +17,12 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        User, on_delete=models.CASCADE, related_name='%(class)ss')
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
     group = models.ForeignKey(
         Group, on_delete=models.SET_NULL,
-        related_name='posts', blank=True, null=True
+        related_name='%(class)ss', blank=True, null=True
     )
 
     def __str__(self):
@@ -31,9 +31,9 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User, on_delete=models.CASCADE, related_name='%(class)ss')
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
+        Post, on_delete=models.CASCADE, related_name='%(class)ss')
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
@@ -41,9 +41,9 @@ class Comment(models.Model):
 
 class Follow(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='followers')
+        User, on_delete=models.CASCADE, related_name='%(class)ss')
     following = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='followeds')
+        User, on_delete=models.CASCADE, related_name='%(class)ss')
 
     class Meta:
         unique_together = ('user', 'following')
